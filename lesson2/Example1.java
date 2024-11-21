2d024aecbd991626708bbadc8f17df9c4cb02e56
Nazarka555 <104325231+Nazarka555@users.noreply.github.com>
2024-11-21 20:16:50 +0200

    Add files via upload

---
+++ lesson2/Example1.java
@@ -0,0 +1,12 @@
+package com.teleran.base.lesson2;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.print("Hi");
+        System.out.print(" my");
+        System.out.print(" name");
+        System.out.print(" is");
+        System.out.print(" Nazarii");
+        System.out.println(" !");
+    }
+}
